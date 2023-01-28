fd7e72c27fccfb7342d6cb7ffb3f406dcbb4459f
Kirtiraj <refreshmymind0@gmail.com>
2023-01-28 16:19:48 +0530

    added .h file

---
+++ ZMPT101B.h
@@ -0,0 +1,26 @@
+#ifndef ZMPT101B_H
+#define ZMPT101B_H
+
+#include <Arduino.h>
+
+#define adcres 1023.0
+#define ip_vin 5.0
+#define default_frequency 50
+
+class ZMPT101B
+{
+public:
+    ZMPT101B(uint8_t _pin);
+    int calibration();
+    void setzero(int _zero);
+    void setSensitivity(float sense);
+    float getVdc();
+    float getVac(uint16_t ipfrequency = 50);
+
+private:
+    uint8_t pin;
+    float sensitivity;
+    int z = 512;
+};
+
+#endif
